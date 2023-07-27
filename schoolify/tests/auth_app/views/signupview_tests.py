49cba59ce9de08af07f6725021bb00b0f5429a8d
Bogomila <bkatsarska@abv.bg>
2023-07-27 16:42:26 +0300

    signupview_tests.py

--- schoolify/tests/auth_app/views/signupview_tests.py
+++ schoolify/tests/auth_app/views/signupview_tests.py
@@ -1,39 +1,41 @@
-# from django.test import TestCase
-# from django.urls import reverse
-#
-# from schoolify.auth_app.models import Profile
-#
-#
-# class SignUpViewTests(TestCase):
-#     VALID_PROFILE_DATA = {
-#         'personal_number': '1234567890',
-#         'password': '123qwe!@#',
-#         'first_name': 'Bogomila',
-#         'last_name': 'Katsarska',
-#         'school_grade': 9,
-#     }
-#
-#
-#     def test_create_profile__when_all_valid__expect_to_create(self):
-#         self.client.post(reverse('sign up'), data=self.VALID_PROFILE_DATA)
-#
-#         profile = Profile.objects.first()
-#         # appuser = AppUser.objects.first()
-#         self.assertIsNotNone(profile.pk)
-#         # self.assertEqual(self.VALID_PROFILE_DATA['personal_number'], appuser.personal_number)
-#         # self.assertEqual(self.VALID_PROFILE_DATA['password'], appuser.password)
-#         self.assertEqual(self.VALID_PROFILE_DATA['first_name'], profile.first_name)
-#         self.assertEqual(self.VALID_PROFILE_DATA['last_name'], profile.last_name)
-#         self.assertEqual(self.VALID_PROFILE_DATA['school_grade'], profile.school_grade)
-#
-#
-#     # def test_create_profile__when_all_valid__expect_to_redirect_to_details(self):
-#     #     response = self.client.post(
-#     #         reverse('create profile'),
-#     #         data=self.VALID_PROFILE_DATA,
-#     #     )
-#     #
-#     #     profile = Profile.objects.first()
-#     #
-#     #     expected_url = reverse('details profile', kwargs={'pk': profile.pk})
-#     #     self.assertRedirects(response, expected_url)
+from django.contrib.auth.hashers import make_password
+from django.test import TestCase
+from django.urls import reverse
+
+from schoolify.auth_app.models import Profile, AppUser
+
+
+class SignUpViewTests(TestCase):
+    VALID_PROFILE_DATA = {
+        'personal_number': 1234567890,
+        'password1': '123qwe!@#',
+        'password2': '123qwe!@#',
+        'first_name': 'Bogomila',
+        'last_name': 'Katsarska',
+        'school_grade': 9,
+    }
+
+
+    def test_create_profile__when_all_valid__expect_to_create(self):
+        self.client.post(reverse('sign up'), data=self.VALID_PROFILE_DATA)
+
+        profile = Profile.objects.first()
+        appuser = AppUser.objects.first()
+        self.assertIsNotNone(profile.pk)
+        self.assertIsNotNone(appuser.pk)
+        self.assertEqual(self.VALID_PROFILE_DATA['personal_number'], appuser.personal_number)
+        #TODO: ask about hashing
+        self.assertEqual(make_password(self.VALID_PROFILE_DATA['password2']), appuser.password)
+        self.assertEqual(self.VALID_PROFILE_DATA['first_name'], profile.first_name)
+        self.assertEqual(self.VALID_PROFILE_DATA['last_name'], profile.last_name)
+        self.assertEqual(self.VALID_PROFILE_DATA['school_grade'], profile.school_grade)
+
+
+    def test_create_profile__when_all_valid__expect_to_redirect_to_index(self):
+        response = self.client.post(
+            reverse('sign up'),
+            data=self.VALID_PROFILE_DATA,
+        )
+
+        expected_url = reverse('index')
+        self.assertRedirects(response, expected_url)
